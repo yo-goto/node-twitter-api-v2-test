af68b23097e3e978751f09ecadff1bdf4ee78e61
yo-goto <50942816+yo-goto@users.noreply.github.com>
2022-06-13 00:08:53 +0900

    リファクタリング: 関数に切り出して例外処理を分かりやすくした

--- index.js
+++ index.js
@@ -1,53 +1,73 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 import { TwitterApi } from "twitter-api-v2";
 import { stringify } from "csv-stringify/sync";
 import { join } from "path";
 import { format } from "date-fns";
-import 'dotenv/config'; // ESmodule形式だとこうなる
-
-const fsPromises = fs.promises;
+import "dotenv/config"; // ESmodule形式だとこうなる
 
 // .env ファイルの読み込みがうまく行かなければエラー吐かせて終了
 const token = process.env.BEARER_TOKEN;
 if (typeof token === "undefined") {
-  console.error('Error: "BEARER_TOKEN" is not set.');
+  console.error('エラー: "BEARER_TOKEN" がセットされていません');
   process.exit(1);
 }
 const client = new TwitterApi(token);
 
 // クエリ構築
 const queryWords = "JavaScript lang:ja";
-const options = { 
-  "max_results": 10, // 最大取得ツイート数
+const options = {
+  max_results: 10, // 最大取得ツイート数
 };
 
+const jsTweets = await fetchTweets(queryWords, options);
+const csvString = stringify(jsTweets.tweets, {
+  header: false,
+});
+
+const timeStamp = format(new Date(), "yyyy-MM-dd-kk-mm-ss");
+const fileName = `${timeStamp}.csv`;
+const dirName = "results";
+const dirPath = join(process.cwd(), dirName);
+// process.cwd() でカレントディレクトリのパスを取得して join でパスを結合
+
+ensureDirSyncFn(dirPath);
 try {
-  const jsTweets = await client.v2.search(queryWords, options);
-  // 返り値が TweetSearchRecentV2Paginetor が入る Promise 
-  const csvString = stringify(jsTweets.tweets, {
-    header: false,
-  });
-
-  const timeStamp = format(new Date(), "yyyy-MM-dd-kk-mm-ss");
-  const fileName = `${timeStamp}.csv`;
-
-  const dirName = "results";
-  const dirPath = join(process.cwd(), dirName);
-  // process.cwd() でカレントディレクトリのパスを取得して join でパスを結合
-
-  try {
-    // ディレクトリの存在を保証する
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath);
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-
-  await fsPromises.writeFile(`${dirPath}/${fileName}`, csvString);
-  console.log("--> succeed writing data!")
+  fs.writeFileSync(`${dirPath}/${fileName}`, csvString);
+  console.log("ファイルの書き込みに成功しました");
 } catch (err) {
   console.error(err);
+}
+
+/**
+ * ツイートの取得
+ * @param {string} query
+ * @param {object} option
+ * @returns {Promise<TweetSearchRecentV2Paginator|void>} 取得した tweet
+ */
+async function fetchTweets(query, option) {
+  try {
+    return await client.v2.search(query, option);
+    // 返り値が TweetSearchRecentV2Paginetor が入る Promise
+  } catch (e) {
+    console.log("データの取得に失敗しました");
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+/**
+ * 指定したディレクトリがなければ作成する
+ * @param {string} directory ディレクトリパス
+ */
+function ensureDirSyncFn(directory) {
+  if (!fs.existsSync(directory)) {
+    try {
+      fs.mkdirSync(directory);
+    } catch (err) {
+      console.log("ディレクトリの作成に失敗しました");
+      console.error(err);
+      process.exit(1);
+    }
+  }
 }
 
